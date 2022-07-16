04de32d657d3713c823e182b9444ec26c330b448
document10 <popescul2222georgeteodor@gmail.com>
2022-07-16 14:38:22 +0300

    Add files via upload

---
+++ dotfiles/xmonad/xmonad_autostart.sh
@@ -0,0 +1,10 @@
+xmonad --recompile
+xmonad --restart
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+picom -f &
+nitrogen --restore &
+conky &
+polybar -r -c .config/polybar/xmonad.ini &
+volumeicon &
+nm-applet &
+blueman-applet &
