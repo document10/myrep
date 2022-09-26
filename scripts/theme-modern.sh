2c29065de6268f4964cbc06a7a9b5994ee2de353
document10 <popescul2222georgeteodor@gmail.com>
2022-09-26 21:44:43 +0300

    Update theme-modern.sh

--- scripts/theme-modern.sh
+++ scripts/theme-modern.sh
@@ -5,5 +5,9 @@ cd ..
 git clone https://github.com/vinceliuice/Tela-circle-icon-theme
 cd Tela-circle-icon-theme
 ./install.sh -a
+cd ..
+git clone https://github.com/vinceliuice/Vimix-cursors
+cd Vimix-cursors
+sudo ./install.sh
 cd /usr/share/backgrounds
 sudo git clone https://gitlab.com/dwt1/wallpapers
