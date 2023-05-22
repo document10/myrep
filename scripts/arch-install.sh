79fe2591491dd5786d8e53cd1922c94ae2625594
document10 <popescul2222georgeteodor@gmail.com>
2023-05-22 13:18:31 +0300

    Update arch-install.sh
    updated the script,hasn't been touched in a long time

--- scripts/arch-install.sh
+++ scripts/arch-install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ln -sf /usr/share/zoneinfo/Europe/Bucharest /etc/localtime
-hwclock --systohc
-locale-gen
+hwclock --systohc --utc
+echo "Configuring locales"
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "KEYMAP=us" >> /etc/vconsole.conf
+locale-gen
 echo "Configuring network"
 echo "archmc" >> /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
@@ -11,13 +13,11 @@ echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 archmc.localdomain archmc" >> /etc/hosts
 cat /etc/hosts
 echo "Root password:"
-passwd
+passwd root
 echo "Installing base packages"
-pacman -Syu --noconfirm linux-firmware git neofetch networkmanager reflector base-devel linux-headers ipset os-prober ntfs-3g terminus-font openssh bash-completion dnsutils ufw iptables micro exa ed gd lldb gdb crda ipcalc strace iwd lsof htop db
+pacman -Syu --noconfirm linux-firmware git neofetch networkmanager reflector base-devel linux-headers ipset os-prober ntfs-3g terminus-font openssh bash-completion dnsutils ufw iptables micro exa ed gd lldb gdb crda ipcalc strace iwd lsof htop db efibootmgr grub
 
-#echo "Configuring grub bootloader"
-#uefi
-pacman -S efibootmgr grub
+echo "Configuring grub bootloader (efi)" 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
@@ -25,19 +25,22 @@ echo "Configuring base services"
 systemctl enable --now NetworkManager
 systemctl enable --now iwd
 systemctl enable --now sshd
+echo "Configuring firewall"
 systemctl enable --now iptables
 systemctl enable --now ip6tables
 systemctl enable --now ufw
 ufw enable
 ufw allow ssh
 ufw status
+
 echo "Creating a new user"
 useradd -m dxvk
 echo "Password for dxvk:"
 passwd dxvk
 echo "dxvk ALL=(ALL) ALL" >> /etc/sudoers.d/doc10
-echo "dxvk now has sudo privilleges."
 usermod -aG wheel,audio,video,optical,storage dxvk
+echo "dxvk now has sudo privilleges."
+
 echo "Final configuration"
 mkinitcpio -P
 echo "Installation complete."
